4fae1bebd14b0b01f8f03ed95ee4227ef4f14075
TuPu <141525093+TuuPuu@users.noreply.github.com>
2024-01-03 12:20:30 +0000

    created cleaner code by adding in functions

--- src/data-test.js
+++ src/data-test.js
@@ -1,3 +1,4 @@
+// CLEANER CODE
 // LIST OF CITIES AND THIER DETAILS (OBJECTS)
 let cities = [
   { city: "London", temperature: 10, faren: 13, humidity: 23 },
@@ -17,64 +18,70 @@ let userCity = "";
 let userInput = prompt("Enter your city");
 
 // FIX USER INPUT
-let userCityLowerCase = userInput.trim().toLowerCase();
+function fixUserInput() {
+  let userCityLowerCase = userInput.trim().toLowerCase();
 
-// CAPITALISE USER INPUT
-let firstLetter = userCityLowerCase.charAt(0).toUpperCase();
-let restOfWord = userCityLowerCase.slice(1);
-userCity = firstLetter + restOfWord;
+  // CAPITALISE USER INPUT
+  let firstLetter = userCityLowerCase.charAt(0).toUpperCase();
+  let restOfWord = userCityLowerCase.slice(1);
+  return (userCity = firstLetter + restOfWord);
+}
+fixUserInput();
 
 // DECIDE OUTPUT FOR USER
-if (userCity === cities[0].city) {
-  chosenCity = cities[0].city;
-  chosenTemperature = cities[0].temperature;
-  chosenFaren = cities[0].faren;
-  chosenHumidity = cities[0].humidity;
-  alert(
-    `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
-  );
-} else if (userCity === cities[1].city) {
-  chosenCity = cities[1].city;
-  chosenTemperature = cities[1].temperature;
-  chosenFaren = cities[1].faren;
-  chosenHumidity = cities[1].humidity;
-  alert(
-    `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
-  );
-} else if (userCity === cities[2].city) {
-  chosenCity = cities[2].city;
-  chosenTemperature = cities[2].temperature;
-  chosenFaren = cities[2].faren;
-  chosenHumidity = cities[2].humidity;
-  alert(
-    `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
-  );
-} else if (userCity === cities[3].city) {
-  chosenCity = cities[3].city;
-  chosenTemperature = cities[3].temperature;
-  chosenFaren = cities[3].faren;
-  chosenHumidity = cities[3].humidity;
-  alert(
-    `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
-  );
-} else if (userCity === cities[4].city) {
-  chosenCity = cities[4].city;
-  chosenTemperature = cities[4].temperature;
-  chosenFaren = cities[4].faren;
-  chosenHumidity = cities[4].humidity;
-  alert(
-    `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
-  );
-} else if (userCity === cities[5].city) {
-  chosenCity = cities[5].city;
-  chosenTemperature = cities[5].temperature;
-  chosenFaren = cities[5].faren;
-  chosenHumidity = cities[5].humidity;
-  alert(
-    `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
-  );
-} else {
-  alert(
-    `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${userInput}`
-  );
+function decideOutput() {
+  if (userCity === cities[0].city) {
+    chosenCity = cities[0].city;
+    chosenTemperature = cities[0].temperature;
+    chosenFaren = cities[0].faren;
+    chosenHumidity = cities[0].humidity;
+    alert(
+      `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
+    );
+  } else if (userCity === cities[1].city) {
+    chosenCity = cities[1].city;
+    chosenTemperature = cities[1].temperature;
+    chosenFaren = cities[1].faren;
+    chosenHumidity = cities[1].humidity;
+    alert(
+      `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
+    );
+  } else if (userCity === cities[2].city) {
+    chosenCity = cities[2].city;
+    chosenTemperature = cities[2].temperature;
+    chosenFaren = cities[2].faren;
+    chosenHumidity = cities[2].humidity;
+    alert(
+      `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
+    );
+  } else if (userCity === cities[3].city) {
+    chosenCity = cities[3].city;
+    chosenTemperature = cities[3].temperature;
+    chosenFaren = cities[3].faren;
+    chosenHumidity = cities[3].humidity;
+    alert(
+      `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
+    );
+  } else if (userCity === cities[4].city) {
+    chosenCity = cities[4].city;
+    chosenTemperature = cities[4].temperature;
+    chosenFaren = cities[4].faren;
+    chosenHumidity = cities[4].humidity;
+    alert(
+      `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
+    );
+  } else if (userCity === cities[5].city) {
+    chosenCity = cities[5].city;
+    chosenTemperature = cities[5].temperature;
+    chosenFaren = cities[5].faren;
+    chosenHumidity = cities[5].humidity;
+    alert(
+      `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
+    );
+  } else {
+    alert(
+      `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${userInput}`
+    );
+  }
 }
+decideOutput();
