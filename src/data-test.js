434c76a5c77aefa876348c82e1f91c047d4239c9
TuPu <141525093+TuuPuu@users.noreply.github.com>
2024-01-03 14:55:16 +0000

    Corrected data for code and added in calculation for Fahenheit

--- src/data-test.js
+++ src/data-test.js
@@ -1,12 +1,12 @@
 // CLEANER CODE
 // LIST OF CITIES AND THIER DETAILS (OBJECTS)
 let cities = [
-  { city: "London", temperature: 10, faren: 13, humidity: 23 },
-  { city: "Paris", temperature: 12, faren: 2, humidity: 2 },
-  { city: "New York", temperature: 6, faren: 36, humidity: 45 },
-  { city: "Tokyo", temperature: 24, faren: 34, humidity: 13 },
-  { city: "Lisbon", temperature: 8, faren: 4, humidity: 9 },
-  { city: "Abidjan", temperature: 34, faren: 8, humidity: 21 },
+  { city: "Paris", temperature: 19.7, humidity: 80 },
+  { city: "Tokyo", temperature: 17.3, humidity: 50 },
+  { city: "Lisbon", temperature: 30.2, humidity: 20 },
+  { city: "San Francisco", temperature: 20.9, humidity: 100 },
+  { city: "Oslo", temperature: -5, humidity: 20 },
+  { city: "Abidjan", temperature: 34, humidity: 2 },
 ];
 
 // GET INPUT FROM USER
@@ -32,48 +32,48 @@ fixUserInput();
 function decideOutput() {
   if (userCity === cities[0].city) {
     chosenCity = cities[0].city;
-    chosenTemperature = cities[0].temperature;
-    chosenFaren = cities[0].faren;
+    chosenTemperature = Math.round(cities[0].temperature);
+    chosenFaren = Math.round((cities[0].temperature * 9) / 5 + 32);
     chosenHumidity = cities[0].humidity;
     alert(
       `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
     );
   } else if (userCity === cities[1].city) {
     chosenCity = cities[1].city;
-    chosenTemperature = cities[1].temperature;
-    chosenFaren = cities[1].faren;
+    chosenTemperature = Math.round(cities[1].temperature);
+    chosenFaren = Math.round((cities[1].temperature * 9) / 5 + 32);
     chosenHumidity = cities[1].humidity;
     alert(
       `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
     );
   } else if (userCity === cities[2].city) {
     chosenCity = cities[2].city;
-    chosenTemperature = cities[2].temperature;
-    chosenFaren = cities[2].faren;
+    chosenTemperature = Math.round(cities[2].temperature);
+    chosenFaren = Math.round((cities[2].temperature * 9) / 5 + 32);
     chosenHumidity = cities[2].humidity;
     alert(
       `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
     );
   } else if (userCity === cities[3].city) {
     chosenCity = cities[3].city;
-    chosenTemperature = cities[3].temperature;
-    chosenFaren = cities[3].faren;
+    chosenTemperature = Math.round(cities[3].temperature);
+    chosenFaren = Math.round((cities[3].temperature * 9) / 5 + 32);
     chosenHumidity = cities[3].humidity;
     alert(
       `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
     );
   } else if (userCity === cities[4].city) {
     chosenCity = cities[4].city;
-    chosenTemperature = cities[4].temperature;
-    chosenFaren = cities[4].faren;
+    chosenTemperature = Math.round(cities[4].temperature);
+    chosenFaren = Math.round((cities[4].temperature * 9) / 5 + 32);
     chosenHumidity = cities[4].humidity;
     alert(
       `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
     );
   } else if (userCity === cities[5].city) {
     chosenCity = cities[5].city;
-    chosenTemperature = cities[5].temperature;
-    chosenFaren = cities[5].faren;
+    chosenTemperature = Math.round(cities[5].temperature);
+    chosenFaren = Math.round((cities[5].temperature * 9) / 5 + 32);
     chosenHumidity = cities[5].humidity;
     alert(
       `It is currently ${chosenTemperature}°C (${chosenFaren}°F) in ${chosenCity} and the humidity is ${chosenHumidity}%`
