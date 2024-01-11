760cbc0c749601168d7e3facca2675aee71e6f5c
TuPu <141525093+TuuPuu@users.noreply.github.com>
2024-01-11 02:53:57 +0000

    Created a new function to handle html event changes and keep code clean

--- src/alt.js
+++ src/alt.js
@@ -1,57 +1,63 @@
 // ADDING TIME INTO CODE
-function getDateAndTime() {
-  let newDate = new Date();
+function addTimeOnPage() {
+  function getDateAndTime() {
+    let newDate = new Date();
 
-  // GETTING THE DAY
-  let listOfDays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+    // GETTING THE DAY
+    let listOfDays = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
 
-  // here is the current day
-  let currentDay = listOfDays[newDate.getDay()];
+    // here is the current day
+    let currentDay = listOfDays[newDate.getDay()];
 
-  // GETTING THE TIME
-  let listOfHours = [
-    "00",
-    "01",
-    "02",
-    "03",
-    "04",
-    "05",
-    "06",
-    "07",
-    "08",
-    "09",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "22",
-    "23",
-  ];
-  let currentHour = listOfHours[newDate.getHours()];
-  let currentMinute = newDate.getMinutes();
+    // GETTING THE TIME
+    let listOfHours = [
+      "00",
+      "01",
+      "02",
+      "03",
+      "04",
+      "05",
+      "06",
+      "07",
+      "08",
+      "09",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "20",
+      "21",
+      "22",
+      "23",
+    ];
+    let currentHour = listOfHours[newDate.getHours()];
+    let currentMinute = newDate.getMinutes();
 
-  // here is the current time
-  let currentTime = `${currentHour}:${currentMinute}`;
+    // here is the current time
+    let currentTime = `${currentHour}:${currentMinute}`;
 
-  // FULL TIME AND DATE HERE
-  let dateAndTime = `${currentDay} ${currentTime}`;
-  return dateAndTime;
+    // FULL TIME AND DATE HERE
+    let dateAndTime = `${currentDay} ${currentTime}`;
+    return dateAndTime;
+  }
+  let fetchedDate = getDateAndTime();
+
+  // ________________________________________________________________
+  let currentDate = document.querySelector("#current-date");
+  currentDate.innerHTML = `${fetchedDate}`;
 }
-getDateAndTime();
-// ________________________________________________________________
+addTimeOnPage();
