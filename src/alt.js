80afe93b40691ee5bca63b399d9e7abc505e2e80
TuPu <141525093+TuuPuu@users.noreply.github.com>
2024-01-11 02:43:18 +0000

    Added date and time code into a function in order to clean up the code

--- src/alt.js
+++ src/alt.js
@@ -1,52 +1,57 @@
 // ADDING TIME INTO CODE
+function getDateAndTime() {
+  let newDate = new Date();
 
-let newDate = new Date();
-console.log(newDate);
+  // GETTING THE DAY
+  let listOfDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-// GETTING THE DAY
-let listOfDays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-// here is the current day
-let currentDay = listOfDays[newDate.getDay()];
-console.log(currentDay);
+  // here is the current day
+  let currentDay = listOfDays[newDate.getDay()];
 
-// GETTING THE TIME
-let listOfHours = [
-  "00",
-  "01",
-  "02",
-  "03",
-  "04",
-  "05",
-  "06",
-  "07",
-  "08",
-  "09",
-  "10",
-  "11",
-  "12",
-  "13",
-  "14",
-  "15",
-  "16",
-  "17",
-  "18",
-  "19",
-  "20",
-  "21",
-  "22",
-  "23",
-];
-let currentHour = listOfHours[newDate.getHours()];
-let currentMinute = newDate.getMinutes();
+  // GETTING THE TIME
+  let listOfHours = [
+    "00",
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+  ];
+  let currentHour = listOfHours[newDate.getHours()];
+  let currentMinute = newDate.getMinutes();
 
-// here is the current time
-let currentTime = `${currentHour}:${currentMinute}`;
-console.log(currentTime);
+  // here is the current time
+  let currentTime = `${currentHour}:${currentMinute}`;
+
+  // FULL TIME AND DATE HERE
+  let dateAndTime = `${currentDay} ${currentTime}`;
+  return dateAndTime;
+}
+getDateAndTime();
+// ________________________________________________________________
